36586fba7511ebc7553c01e0a7b73bd6ee6135ef
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-02 23:02:38 +0530

    add

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 const page = () => {
   return (
-  <div className="w-full max-w-[450px] h-256 px-10 py-20 bg-white inline-flex flex-col justify-start items-center gap-2.5">
-  <div className="self-stretch flex flex-col justify-start items-center gap-7">
-    <h1 className="self-stretch text-center justify-start text-black text-5xl font-bold font-['Inria_Serif']"> To do List</h1>
-    <div className="self-stretch inline-flex justify-center items-center gap-8 flex-wrap content-center">
-      <div className="inline-flex flex-col justify-start items-center gap-2.5">
-        <input type="text" placeholder="Enter task"  className="w-2xl h-20 rounded-[10px] placeholder:text-2xl text-3xl px-6 border-[1.50px] border-black" />
+ <div className="w-[1440px] h-[1024px] px-40 py-12 bg-white inline-flex justify-center items-start gap-2.5">
+  <div className="flex-1 h-64 inline-flex flex-col justify-start items-center gap-12">
+    <div className="self-stretch text-center justify-start text-black text-5xl font-bold font-['Inria_Serif']">To do List</div>
+    <div className="self-stretch px-9 inline-flex justify-center items-start gap-10">
+      <div className="flex-1 inline-flex flex-col justify-start items-center">
+        <div className="self-stretch h-16 rounded-lg border-[1.50px] border-black" />
       </div>
-      <button className="px-4 py-2.5 w-64 h-20 border-2 border-amber-50 bg-red-800 rounded-xl text-white text-5xl cursor-pointer hover:ring-2 ring-black font-bold font-['Inria_Serif']">Add Task</button>
+      <div className="px-2.5 py-1 bg-red-800 rounded-lg flex justify-center items-center gap-2.5">
+        <div className="text-center justify-start text-white text-5xl font-bold font-['Inria_Serif']">Add task</div>
+      </div>
     </div>
   </div>
 </div>
