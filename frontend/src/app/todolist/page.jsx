68e303c33a79198009ba53eb182ccb27c6c6e306
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-02 23:51:33 +0530

    add

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -5,6 +5,15 @@ const page = () => {
  <div className="w-full  px-40 py-12 bg-white inline-flex justify-center items-start gap-2.5">
   <div className="flex-1 h-64 inline-flex flex-col justify-start items-center gap-12">
     <div className="self-stretch text-center justify-start text-black text-5xl font-bold font-['Inria_Serif']">To do List</div>
+<div className="active:md self-stretch inline-flex flex-col justify-start items-center gap-10">
+  <div className="self-stretch px-11 flex flex-col justify-start items-center">
+    <div className="self-stretch h-16 rounded-lg border-[1.50px] border-black" />
+  </div>
+  <div className="size- px-2.5 py-1 bg-red-800 rounded-lg inline-flex justify-center items-center gap-2.5">
+    <div className="text-center justify-start text-white text-5xl font-bold font-['Inria_Serif']">Add task</div>
+  </div>
+</div>
+
     <div className="self-stretch px-9 inline-flex md:flex-row flex-col justify-center md:items-start items-center gap-10">
       <div className="flex-1 inline-flex flex-col justify-start items-center">
         <input placeholder="enter the task" className="px-6 text-2xl  self-stretch h-16 rounded-lg border-[1.50px] border-black" />
