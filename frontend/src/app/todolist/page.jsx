7440c03749aa03315562c44a323a0fa1e58780c5
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-04 19:46:38 +0530

    add

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -1,25 +1,21 @@
 import React from 'react'
 
-const page = () => {
+const todolist = () => {
   return (
-    
- <div className="flex justify-center items-start w-full  px-40 py-12 bg-white  gap-2.5">
-  <div className="flex-1 h-64 flex flex-col justify-start items-center gap-12">
-    <div className="self-stretch text-center justify-start text-black text-5xl font-bold font-['Inria_Serif']">To do List</div>
-    <div className="self-stretch px-9 flex md:flex-row flex-col justify-center items-center gap-10">
-      <div className="flex-1 flex flex-col  justify-start items-center">
-        <input placeholder="enter the task" className="px-6 text-2xl  self-stretch h-16 rounded-lg border-[1.50px] border-black" />
+   <div className="w-full px-40 py-12 bg-white inline-flex justify-center items-start gap-2.5">
+  <div className="flex-1 h-64 inline-flex flex-col justify-start items-center gap-12">
+    <h1 className="self-stretch text-center justify-start text-black text-5xl font-bold font-['Inria_Serif']">To do List</h1>
+    <div className="self-stretch px-9 inline-flex justify-center items-start gap-10">
+      <div className="flex-1 inline-flex flex-col justify-start items-center">
+        <div className="self-stretch h-16 rounded-lg border-[1.50px] border-black" />
       </div>
-      <div className=" flex px-6 h-16 bg-red-800 rounded-lg  justify-center items-center gap-2.5 border-2 border-white hover:ring-2 ring-black cursor-pointer">
-        <button className="text-center  justify-start text-white text-5xl font-bold font-['Inria_Serif'] cursor-pointer ">Add task</button>
+      <div className="px-2.5 py-0.5 bg-red-800 rounded-lg flex justify-center items-center gap-2.5">
+        <button className="text-center justify-start text-white text-5xl font-bold font-['Inria_Serif']">Add task</button>
       </div>
     </div>
   </div>
 </div>
-
-
-
   )
 }
 
-export default page
+export default todolist
