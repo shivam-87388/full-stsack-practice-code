2e115f082fc988f9be85d431bbcac8e0b41fb4b1
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-04 22:59:47 +0530

    add

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -1,12 +1,16 @@
+'use client'
 import React from 'react'
+const  handelclick = ()=>{
+  alert("angry")
+};
 
 const todolist = () => {
   return (
    <div className=" items-center justify-center p-6 -full ">
     <h1 className="text-center text-black text-5xl font-bold font-['Inria_Serif'] p-2">To Do list</h1>
-    <div className="flex flex-col md:flex-row  justify-center items-center gap-4">
-      <input type="text" placeholder="enter task" className="md:w-lg w-64  py-2 px-2.5 text-2xl rounded-lg border-[1.50px] border-black"></input>
-      <button className="px-2.5 py-1 bg-red-800 rounded-lg text-white text-5xl font-bold font-['Inria_Serif']">Add task</button>
+    <div className="flex flex-col md:flex-row justify-center items-center gap-4">
+      <input type="text" placeholder="enter task" className="md:w-lg w-64 py-2.5 px-2.5 text-2xl rounded-lg border-[1.50px] border-black"></input>
+      <button onClick={handelclick} className="flex items-center justify-center px-2.5 py-1 text-nowrap bg-red-800 rounded-lg text-white text-5xl  font-bold font-['Inria_Serif']">Add task</button>
     </div>
    </div>
     
