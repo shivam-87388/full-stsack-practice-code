2808c0ad4a188c8fef7f0949ec05f8868975ff8d
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-04 20:59:14 +0530

    add

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 
 const todolist = () => {
   return (
-   <div className="w-full px-40 py-12 bg-white flex justify-center items-start gap-2.5">
-  <div className="flex-1 h-64 inline-flex flex-col justify-start items-center gap-12">
-    <h1 className="flex  text-center justify-start text-black text-5xl font-bold font-['Inria_Serif']">To do List</h1>
-    <div className="self-stretch  inline-flex justify-center items-start gap-10">
-      <div className="flex-1 inline-flex flex-col justify-start items-center">
-        <div className="self-stretch h-16 rounded-lg border-[1.50px] border-black" />
+    <div className="flex justify-center items-start w-full px-40 py-12 bg-white  gap-2.5">
+      <div className="flex h-64 w-full lg:flex-col flex-row justify-start items-center gap-12">
+        <h1 className="flex justify-start items-center flex-col text-black text-5xl font-bold font-['Inria_Serif'] gap-4">
+          To do List
+        <div className="flex justify-center items-start gap-10">
+          <div className="flex-1 inline-flex flex-col justify-start items-center">
+            <input className="self-stretch h-16 rounded-lg border-[1.50px] border-black" />
+          </div>
+          <button className="flex items-center justify-center  bg-red-800 rounded-lg text-center text-white text-5xl font-bold font-['Inria_Serif']">
+            Add task
+          </button>
+        </div>
+        </h1>
       </div>
-      
-        <button className="flex items-center justify-center px-2.5 py-2.5 bg-red-800 rounded-lg text-center text-white text-5xl font-bold font-['Inria_Serif']">Add task</button>
-      
     </div>
-  </div>
-</div>
-  )
+  );
 }
 
 export default todolist
