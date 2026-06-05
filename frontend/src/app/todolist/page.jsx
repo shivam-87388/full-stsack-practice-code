e98721374f867f8d28b9fc1be063e5bfa8a91b0a
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-05 23:51:54 +0530

    add

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -4,22 +4,22 @@ import { useState } from "react";
 import toast, { Toaster } from 'react-hot-toast';
 
 const handelsubmit = ()=>{
+};
 
-  const [set, setname] = useState("");
-}
 const  handelclick = ()=>{
   
- 
- 
-
+  
+  
+  
 };
 
 const todolist = () => {
+  const [set, settask] = useState("");
   return (
    <div className=" items-center justify-center p-6 -full ">
     <h1 className="text-center text-black text-5xl font-bold font-['Inria_Serif'] p-2">To Do list</h1>
     <div className="flex flex-col md:flex-row justify-center items-center gap-4">
-      <input type="text" onplaceholder="enter task" className="md:w-lg w-64 py-2.5 px-2.5 text-2xl rounded-lg border-[1.50px] border-black"></input>
+      <input onSubmit={handelsubmit} type="text" onplaceholder="enter task" className="md:w-lg w-64 py-2.5 px-2.5 text-2xl rounded-lg border-[1.50px] border-black"></input>
       <button onClick={handelclick} className="flex items-center justify-center px-2.5 py-1 text-nowrap bg-red-800 rounded-lg text-white text-5xl  font-bold font-['Inria_Serif']">Add task</button>
     </div>
    </div>
@@ -27,4 +27,4 @@ const todolist = () => {
   );
 }
 
-export default todolist
+export default todolist;
