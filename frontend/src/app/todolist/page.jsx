3f78ab04c82c7bd918bb9e73020a2c254870a52c
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-08 00:16:44 +0530

    add

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 
 
+
 const todolist = () => {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
@@ -18,18 +19,19 @@ const todolist = () => {
   };
   return (
     
-   <div className=" items-center justify-center p-6 -full ">
+   <div className=" flex items-center justify-center p-6 flex-col w-full">
     <h1 className="text-center text-black text-5xl font-bold font-['Inria_Serif'] p-2">To Do list</h1>
     <div className="flex flex-col md:flex-row justify-center items-center gap-4">
       <input value={task} onChange={handelchange} type="text" placeholder="enter task" className="md:w-lg w-64 h-14 px-4.5 text-2xl rounded-lg border-2 border-black"></input>
       <button onClick={handelclick} className="flex items-center justify-center px-6 h-14 text-nowrap cursor-pointer hover:ring-2 ring-black bg-red-800 rounded-lg text-white text-xl md:text-2xl border-2 boder-white  font-bold font-['Inria_Serif']">Add task</button>
     </div>
-     <div className="flex items-center justify-center flex-col gap-4 mt-5 w-full ">
+     <div className="flex items-center justify-center flex-col gap-4 mt-5 w-1/2 bg-amber-200 rounded-2xl px-4 py-3">
        {tasks.map((item, index) => {
-        return <p key={index} className="text-2xl border-2 text-white border-white w-3/5 rounded-2xl p-2.5 ">{item}</p>
+        return <p key={index} className="w-full text-2xl border-2 text-black border-black  rounded-2xl p-2.5 ">{item}</p>
 
        })}
       </div>
+     
    </div>
     
   );
