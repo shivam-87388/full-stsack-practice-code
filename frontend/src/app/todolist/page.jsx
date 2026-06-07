5347d8e5da1e29f3e646b91245461f28b1bc33b2
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-07 11:55:00 +0530

    add

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import { useState } from "react";
-import toast, { Toaster } from 'react-hot-toast';
+
 
 
 
@@ -23,11 +23,8 @@ const todolist = () => {
     <div className="flex flex-col md:flex-row justify-center items-center gap-4">
       <input onChange={handelchange} type="text" placeholder="enter task" className="md:w-lg w-64 py-2.5 px-2.5 text-2xl rounded-lg border-[1.50px] border-black"></input>
       <button onClick={handelclick} className="flex items-center justify-center px-2.5 py-1 text-nowrap cursor-pointer hover:ring-2 ring-black bg-red-800 rounded-lg text-white text-5xl border-2 boder-white  font-bold font-['Inria_Serif']">Add task</button>
-      {
-  tasks.map((item, index) => (
-    <p key={index}>{item}</p>
-  ))
-}
+      
+ 
     </div>
    </div>
     
