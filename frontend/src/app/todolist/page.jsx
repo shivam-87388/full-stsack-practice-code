72819cf01821a627dfa633c28ffa42bf9a9357d1
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-09 20:54:17 +0530

    add

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -19,6 +19,10 @@ const todolist = () => {
     setTasks([...tasks, task])
    console.log(...tasks,task);
   };
+
+  const handeldelete = ()=>{
+
+  };
   return (
     
    <div className=" flex items-center justify-center p-6 flex-col w-full">
@@ -27,13 +31,18 @@ const todolist = () => {
       <input value={task} onChange={handelchange} type="text" placeholder="enter task" className="md:w-lg w-64 h-14 px-4.5 text-2xl rounded-lg border-2 border-black"></input>
       <button onClick={handelclick} className="flex items-center justify-center px-6 h-14 text-nowrap cursor-pointer hover:ring-2 ring-black bg-red-800 rounded-lg text-white text-xl md:text-2xl border-2 boder-white  font-bold font-['Inria_Serif']">Add task</button>
     </div>
+    
      <div className="flex items-center justify-center flex-col gap-4 mt-5 lg:w-1/2 w-full bg-amber-200 rounded-2xl px-4 py-3">
      
        {tasks.map((item, index) => {
-        return <p key={index} className="w-full text-2xl border-2 text-black border-black  rounded-2xl p-2.5 ">{item}</p>
+        return <p key={index} className="flex justify-between w-full text-2xl border-2 text-black border-black  rounded-2xl p-2.5 ">{item}
+        <button  onClick={handeldelete}className="bg-black rounded-md p-1.5 hover:bg-gray-500 cursor-pointer"><IconTrash color='white'/></button>
+        </p>
         
        })}
       </div>
+    
+     
      
    </div>
     
