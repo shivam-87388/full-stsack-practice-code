48843b6928b7c691f1765b2a551d007965869997
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-11 21:59:16 +0530

    add

--- frontend/src/app/counter/page.jsx
+++ frontend/src/app/counter/page.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useState } from 'react';
+
+const [count, setCount] = useState(0);
 
 const counter = () => {
   return (
     <div className=" flex flex-col justify-center items-center w-full">
-        <h1></h1>
-        <button className="bg-emerald-700 text-2xl text-white px-2 py-2.5">Count</button>
+        <h1>{count}</h1>
+        <button className="flex justify-center items-center bg-emerald-700 text-2xl text-white px-3 py-2.5 rounded-lg">Count</button>
 
     </div>
   )
