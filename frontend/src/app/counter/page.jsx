be861699fd417462df00d11fe8ef85096f532729
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-11 21:47:13 +0530

    add

--- frontend/src/app/counter/page.jsx
+++ frontend/src/app/counter/page.jsx
@@ -4,7 +4,7 @@ const counter = () => {
   return (
     <div className=" flex flex-col justify-center items-center w-full">
         <h1></h1>
-        <button className=''>Count</button>
+        <button className="bg-emerald-700 text-2xl text-white">Count</button>
 
     </div>
   )
