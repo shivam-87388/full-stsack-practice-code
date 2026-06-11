40da4b5e82d8fe6aec71af38aa64ccbe72199f09
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-11 22:39:33 +0530

    add

--- frontend/src/app/counter/page.jsx
+++ frontend/src/app/counter/page.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const counter = () => {
     const [count, setCount] = useState(0);
 const handelClick = ()=>{
-   setCount()
+    setCount(count+1);
 
 
 
@@ -13,7 +13,7 @@ const handelClick = ()=>{
   return (
     <div className="h-screen flex flex-col justify-center items-center w-full">
         <h1 className="text-2xl ">{count}</h1>
-        <button onClick={handelclick} className="flex justify-center items-center bg-emerald-700 text-2xl text-white px-3 py-2.5 rounded-lg">Count</button>
+        <button onClick={handelClick} className="flex justify-center items-center bg-emerald-700 text-2xl text-white px-3 py-2.5 rounded-lg">Count</button>
 
     </div>
   )
