fab059132a51878e0c0d3a648aa5b4274acdd702
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-11 23:41:47 +0530

    add

--- frontend/src/app/input/page.jsx
+++ frontend/src/app/input/page.jsx
@@ -1,10 +1,17 @@
+'use client'
 import React from 'react'
+import { useState } from 'react'
+
 
 const page = () => {
+  const [name, setName] = useState([]);
+  const handelChange = (element)=>{
+
+  };
   return (
     <div className="flex flex-col justify-center items-center w-full h-screen">
       <label  id="fname" className="text-2xl">enter something</label>
-        <input type="text" id="fname" className="w-80 border-2 p-1.5 rounded-md text-2xl"></input>
+        <input  onChange={handelChange} type="text" id="fname" className="w-80 border-2 hover:cursor-pointer p-1.5 rounded-md text-2xl "></input>
     </div>
   )
 }
