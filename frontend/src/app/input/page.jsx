c78d61c7b0abddbf2823e5abd53ccf14a6f9ab22
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-12 23:50:23 +0530

    add

--- frontend/src/app/input/page.jsx
+++ frontend/src/app/input/page.jsx
@@ -33,10 +33,10 @@ const page = () => {
       >
         submit
       </button>
-      <div className="flex justify-center items-center bg-green-800">
+      <div className="flex flex-col justify-center items-center bg-green-800">
         {tasks.map((item, index) => {
           return (
-            <p key={index} className="text-2xl text-white">
+            <p key={index} className="flex justify-center items-center text-2xl text-white w-80 border-2 border-white">
               {item}
             </p>
           );
