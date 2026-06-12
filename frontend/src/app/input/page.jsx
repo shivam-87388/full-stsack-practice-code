b1586ff072cf03eefd55450d0bdae27295767917
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-12 20:21:20 +0530

    add

--- frontend/src/app/input/page.jsx
+++ frontend/src/app/input/page.jsx
@@ -2,16 +2,32 @@
 import React from 'react'
 import { useState } from 'react'
 
-
 const page = () => {
   const [name, setName] = useState([]);
-  const handelChange = (element)=>{
 
+  const handelChange = (e)=>{
+    setName([...name,e.target.value]);
+    console.log([...name,e.target.value]);
   };
+
+  const handelAdd = ()=>{
+ 
+  }
+
+
   return (
-    <div className="flex flex-col justify-center items-center w-full h-screen">
-      <label  id="fname" className="text-2xl">enter something</label>
-        <input  onChange={handelChange} type="text" id="fname" className="w-80 border-2 hover:cursor-pointer p-1.5 rounded-md text-2xl "></input>
+    <div className="flex flex-col justify-center items-center w-full h-screen space-y-2">
+      <label  id="fname" className="text-2xl ">enter something</label>
+        <input key={setName}  onChange={handelChange} type="text" id="fname" className="w-80 border-2 hover:cursor-pointer p-1.5 rounded-md text-2xl "></input>
+        <button onClick={handelAdd} className="flex justify-center items-center text-2xl px-2.5 py-2 bg-amber-400 rounded-md">submit</button>
+
+<div>
+  {name.map((item,index)=>{
+    return
+    <p key={index}>{item}</p>
+
+  })};
+</div>
     </div>
   )
 }
--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -6,35 +6,31 @@ import {IconTrash } from '@tabler/icons-react';
 
 
 const todolist = () => {
-  const [task, setTask] = useState([]);
-  const handelchange = (e)=>{
-    setTask(e.target.value);
-     console.log(e.target.value); 
-  };
-  const  handelclick = ()=>{
-   
+ const [task, setTasks] = useState([]);
 
+ const handelChange =(e)=>{
+  setTasks([...task,e.target.value]);
+  console.log([...task,e.target.value]);
+ };
 
-  const handeldelete = (index)=>{
-   const newarr =  [...tasks]
-   newarr.splice(index,1);
-   setTask(newarr);
+ const  handelclick =()=>{
+  
 
-  };
+ };
   return (
     
    <div className=" flex items-center justify-center p-6 flex-col w-full">
     <h1 className="text-center text-black text-5xl font-bold font-['Inria_Serif'] p-2">To Do list</h1>
     <div className="flex flex-col md:flex-row justify-center items-center gap-4">
-      <input value={task} onChange={handelchange} type="text" placeholder="enter task" className="md:w-lg w-64 h-14 px-4.5 text-2xl rounded-lg border-2 border-black"></input>
-      <button onClick={handelclick} className="flex items-center justify-center px-6 h-14 text-nowrap cursor-pointer hover:ring-2 ring-black bg-red-800 rounded-lg text-white text-xl md:text-2xl border-2 boder-white  font-bold font-['Inria_Serif']">Add task</button>
+      <input value={task} onChange={handelChange} type="text" placeholder="enter task" className="md:w-lg w-64 h-14 px-4.5 text-2xl rounded-lg border-2 border-black"></input>
+      <button onClick={handelclick} className="flex items-center justify-center px-6 h-14 text-nowrap cursor-pointer hover:ring-2 ring-black bg-red-800 rounded-lg text-white text-xl md:text-2xl border-2 border-white font-bold font-['Inria_Serif']">Add task</button>
     </div>
     
      <div className="flex items-center justify-center flex-col gap-4 mt-5 lg:w-1/2 w-full bg-amber-200 rounded-2xl px-4 py-3">
      
        {task.map((item, index) => {
         return <p key={index} className="flex justify-between w-full text-2xl border-2 text-black border-black  rounded-2xl p-2.5 ">{item}
-        <button onClick={() => handeldelete(index)} className="bg-black rounded-md p-1.5 hover:bg-gray-500 cursor-pointer"><IconTrash color='white' size={25}/></button>
+        {/* <button className="bg-black rounded-md p-1.5 hover:bg-gray-500 cursor-pointer"><IconTrash color='white' size={25}/></button> */}
         </p>
         
        })}
