a5e0db465f902a184c6648798bcc55bf5988d082
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-13 21:45:37 +0530

    final todo list

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -17,43 +17,54 @@ const todolist = () => {
     setTasks([...tasks, task]);
     console.log([...tasks, task]);
   };
-  const handelDelete = (index)=>{
+  const handelDelete = (index) => {
     const tasklist = [...tasks];
-    tasklist.splice(index,1);
+    tasklist.splice(index, 1);
     setTasks(tasklist);
   };
 
   return (
-    <div className=" flex items-center justify-center p-6 flex-col w-full">
-      <h1 className="text-center text-black text-5xl font-bold font-['Inria_Serif'] p-2">
-        To Do list
-      </h1>
-      <div className="flex flex-col md:flex-row justify-center items-center gap-4 p-2">
-        <input
-          value={task}
-          onChange={handelChange}
-          type="text"
-          placeholder="enter task"
-          className="md:w-lg w-64 h-14 px-4.5 text-2xl rounded-lg border-2 border-black"
-        ></input>
-        <button
-          onClick={handelAdd}
-          className="flex items-center justify-center px-6 h-14 text-nowrap cursor-pointer hover:ring-2 ring-black bg-red-800 rounded-lg text-white text-xl md:text-2xl border-2 border-white font-bold font-['Inria_Serif']"
-        >
-          Add task
-        </button>
+    <div>
+      <div className=" flex items-center justify-center p-6 flex-col w-full">
+        <h1 className="text-center text-black text-5xl font-bold font-['Inria_Serif'] p-2">
+          To Do list
+        </h1>
+        <div className="flex flex-col md:flex-row justify-center items-center gap-4 p-2">
+          <input
+            value={task}
+            onChange={handelChange}
+            type="text"
+            placeholder="enter task"
+            className="md:w-lg w-64 h-14 px-4.5 text-2xl rounded-lg border-2 border-black"
+          ></input>
+          <button
+            onClick={handelAdd}
+            className="flex items-center justify-center px-6 h-14 text-nowrap cursor-pointer hover:ring-2 ring-black bg-red-800 rounded-lg text-white text-xl md:text-2xl border-2 border-white font-bold font-['Inria_Serif']"
+          >
+            Add task
+          </button>
+        </div>
+        {tasks.length > 0 && (
+          <div className="flex flex-col justify-center items-center gap-2 rounded-lg bg-green-900 w-3/5 p-2 mt-2">
+            {tasks.map((element, index) => {
+              return (
+                <p
+                  key={index}
+                  className="flex justify-between items-center w-full border-white border-2 text-white rounded-md px-2.5 py-2 text-2xl"
+                >
+                  {element}
+                  <button
+                    onClick={() => handelDelete(index)}
+                    className="p-2 rounded-md bg-cyan-500 hover:bg-cyan-600 hover:cursor-pointer"
+                  >
+                    <IconTrash stroke={2} size={25} />
+                  </button>
+                </p>
+              );
+            })}
+          </div>
+        )}
       </div>
-
-      <div className="flex flex-col justify-center items-center gap-2 rounded-lg bg-green-900 w-3/5 p-2 mt-2">
-         {tasks.map((element, index) => {
-        return <p key={index} className="flex justify-between items-center w-full border-white border-2 text-white rounded-md px-2.5 py-2 text-2xl">{element}
-        <button  onClick={() => handelDelete(index)} className="p-2 rounded-md bg-cyan-500 hover:bg-cyan-600 hover:cursor-pointer">
-         <IconTrash stroke={2} size={25} />
-        </button>
-        </p>;
-      })}
-      </div>
-     
     </div>
   );
 };
