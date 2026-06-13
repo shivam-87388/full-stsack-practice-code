bc9c0898881d9f05f7944cf75609efede762adb9
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-13 20:57:49 +0530

    add

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -1,44 +1,45 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 import { useState } from "react";
-import {IconTrash } from '@tabler/icons-react';
-
-
+import { IconTrash } from "@tabler/icons-react";
 
 const todolist = () => {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
 
- const [task, setTask] = useState("");
- const [tasks, setTasks] = useState([]);
+  const handelChange = (e) => {
+    setTask(e.target.value);
+    console.log(e.target.value);
+  };
 
- const handelChange =(e)=>{
-setTasks(e.target.value);
-console.log(e.target.value);
- };
-
- const handelAdd = ()=>{
-  setTasks([])
- }
-
- 
-  
-
+  const handelAdd = () => {
+    if (task.trim() === "") return;
+    setTasks([...tasks, task]);
+    console.log([...tasks, task]);
+  };
 
   return (
-    
-   <div className=" flex items-center justify-center p-6 flex-col w-full">
-    <h1 className="text-center text-black text-5xl font-bold font-['Inria_Serif'] p-2">To Do list</h1>
-    <div className="flex flex-col md:flex-row justify-center items-center gap-4">
-      <input value={task} onChange={handelChange} type="text" placeholder="enter task" className="md:w-lg w-64 h-14 px-4.5 text-2xl rounded-lg border-2 border-black"></input>
-      <button onClick={handelAdd} className="flex items-center justify-center px-6 h-14 text-nowrap cursor-pointer hover:ring-2 ring-black bg-red-800 rounded-lg text-white text-xl md:text-2xl border-2 border-white font-bold font-['Inria_Serif']">Add task</button>
+    <div className=" flex items-center justify-center p-6 flex-col w-full">
+      <h1 className="text-center text-black text-5xl font-bold font-['Inria_Serif'] p-2">
+        To Do list
+      </h1>
+      <div className="flex flex-col md:flex-row justify-center items-center gap-4">
+        <input
+          value={task}
+          onChange={handelChange}
+          type="text"
+          placeholder="enter task"
+          className="md:w-lg w-64 h-14 px-4.5 text-2xl rounded-lg border-2 border-black"
+        ></input>
+        <button
+          onClick={handelAdd}
+          className="flex items-center justify-center px-6 h-14 text-nowrap cursor-pointer hover:ring-2 ring-black bg-red-800 rounded-lg text-white text-xl md:text-2xl border-2 border-white font-bold font-['Inria_Serif']"
+        >
+          Add task
+        </button>
+      </div>
     </div>
-    
-    
-      
-     
-     
-   </div>
-    
   );
-}
+};
 
 export default todolist;
