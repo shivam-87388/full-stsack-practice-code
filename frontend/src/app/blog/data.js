f94906daf24cbbb1d9761085b0c8de6cb23b2384
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-14 20:27:26 +0530

    add

---
+++ frontend/src/app/blog/data.js
@@ -0,0 +1,6 @@
+export const Blogs =[
+    {
+        id: 1
+        tittle: "how to make dynamic route"
+    }
+]
--- frontend/src/app/dark-ligth-toggle/page.jsx
+++ frontend/src/app/dark-ligth-toggle/page.jsx
@@ -5,7 +5,7 @@ const Togglelight = () => {
   return (
     <div className="flex items-center justify-center  w-full h-screen ">
         <div className="flex items-center justify-center bg-amber-100 rounded-full border-2 border-black gap-2 px-3 py-2.5">
-        <button className="hover:cursor-pointer hover:bg-amber-200"><IconSun stroke={2}  size={25}/></button>
+        <button className="hover:cursor-pointer hover:bg-amber-200"><IconSun stroke={2}  size={25} /></button>
         <button className="hover:cursor-pointer hover:fill-black"><IconMoon stroke={2} size={25}/></button>
         </div>
     
