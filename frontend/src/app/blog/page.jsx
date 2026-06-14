344d39318ffd0a0973fd4df33cb9945ff5cfecd0
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-14 21:44:29 +0530

    add

--- frontend/src/app/blog/page.jsx
+++ frontend/src/app/blog/page.jsx
@@ -1,3 +1,14 @@
 import Link from "next/link";
-import { blog } from "./data";
+import { blogs } from "./data";
 
+
+const blog = () => {
+  return (
+    <div className="p-4" >
+        <h1 className="flex items-center justify-center">blogs</h1>
+
+    </div>
+  )
+}
+
+export default blog;
