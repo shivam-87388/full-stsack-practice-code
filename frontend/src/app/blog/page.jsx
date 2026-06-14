57eaae4c5e7e3884c604e8e56160e53c30ae6ad1
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-14 21:46:33 +0530

    add

--- frontend/src/app/blog/page.jsx
+++ frontend/src/app/blog/page.jsx
@@ -4,8 +4,8 @@ import { blogs } from "./data";
 
 const blog = () => {
   return (
-    <div className="p-4" >
-        <h1 className="flex items-center justify-center">blogs</h1>
+    <div className="p-4 bg-green-500 h-screen" >
+        <h1 className="flex items-center justify-center text-3xl text-white font-semibold">Blogs</h1>
 
     </div>
   )
