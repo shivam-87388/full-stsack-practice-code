3d5b42df46e3e21d1efba123dd8e115311326f19
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-14 23:32:51 +0530

    add

--- frontend/src/app/blog/page.jsx
+++ frontend/src/app/blog/page.jsx
@@ -4,18 +4,18 @@ import { blogs } from "./data";
 
 const blog = () => {
   return (
-   <section className="">
-    <h1 className="flex justify-center items-center text-2xl font-bold p-2.5 mt-4"> Blogs</h1>
+   <section className="text-gray-600 body-font bg-green-500">
+    <h1 >Blogs</h1>
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-wrap -m-4">
-       <div className="p-4 md:w-1/3">
+      <div className="p-4 md:w-1/3 bg-blue-500">
         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <img
             className="lg:h-48 md:h-36 w-full object-cover object-center"
             src="https://dummyimage.com/720x400"
             alt="blog"
           />
-          <div className="p-6">
+          <div className="p-6 bg-white">
             <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
               CATEGORY
             </h2>
@@ -74,6 +74,7 @@ const blog = () => {
             </div>
           </div>
         </div>
+      </div>
     </div>
   </div>
 </section>
