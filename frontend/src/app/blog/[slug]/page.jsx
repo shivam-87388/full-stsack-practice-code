615836b7b2178ae7cc914fc1ad52b884473c8933
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-16 23:54:23 +0530

    add

--- frontend/src/app/blog/[slug]/page.jsx
+++ frontend/src/app/blog/[slug]/page.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>page
+    <h1> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed repellendus ipsum tenetur quaerat omnis molestias non ea suscipit voluptatibus, adipisci ut reiciendis corporis autem velit sint expedita. Quisquam, commodi recusandae.</h1>
+    </div>
   )
 }
 
