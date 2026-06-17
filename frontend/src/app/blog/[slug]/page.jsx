80e79a787cbb228d9f7b8cf673183ff76de5019e
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-17 23:29:55 +0530

    add

--- frontend/src/app/blog/[slug]/page.jsx
+++ frontend/src/app/blog/[slug]/page.jsx
@@ -5,6 +5,7 @@ const page = () => {
     <div>
         
         <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, facere cum quaerat expedita dignissimos et consectetur laboriosam exercitationem qui obcaecati nobis voluptatum maxime recusandae, tenetur pariatur officia saepe ipsam porro.</h1>
+        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, facere cum quaerat expedita dignissimos et consectetur laboriosam exercitationem qui obcaecati nobis voluptatum maxime recusandae, tenetur pariatur officia saepe ipsam porro.</h1>
     </div>
   )
 }
