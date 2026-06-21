eef52efa256a826184756c0103675f3cc7f2c9b2
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-21 20:21:48 +0530

    add

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <main className="min-h-screen">
-        <div></div>
+    <main className="min-h-screen bg-gray-300">
+        <div className="bg-white"></div>
     </main>
   )
 }
