c1031229e258defb747d3cd4ed881179da84d4de
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-21 20:29:52 +0530

    add

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <main className="min-h-screen bg-gray-300">
-        <form className="flex felx-cols bg-white ">
+    <main className="flex items-center justify-center min-h-screen bg-gray-300 ">
+        <form className="flex flex-col rounded-2xl bg-white p-4 shadow-2xl w-2xl container">
             <label for="user-email">Email Address</label>
             <input type="email" id="user-email" placeholder="example@gamil.com" required></input>
         </form>
