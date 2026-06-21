7b791f1d80f9459e2492f3db3d8cfa9f29c754d6
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-21 20:52:50 +0530

    add

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -4,8 +4,10 @@ const page = () => {
   return (
     <main className="flex items-center justify-center min-h-screen bg-gray-300 ">
         <form className="flex flex-col rounded-2xl bg-white p-4 shadow-2xl w-2xl container">
-            <label for="user-email">Email Address</label>
-            <input type="email" id="user-email" placeholder="example@gamil.com" required></input>
+            <label htmlFor="user-email">Email Address</label>
+            <input type="email" id="user-email" placeholder="example@gamil.com" required className="w-80 bg-white p-2 border-2 rounded-md"></input>
+            <label htmlFor="userPassword">Password</label>
+            <input type="password" id="userPassword" placeholder="enter password" required></input>
         </form>
     </main>
   )
