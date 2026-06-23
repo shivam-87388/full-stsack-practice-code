e4eb40025e4730c1146f6300ed75eed166b43117
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-23 23:24:40 +0530

    add

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -5,6 +5,7 @@ const page = () => {
     <main className=" flex items-center justify-center min-h-screen bg-gray-200">
       <form className="flex  flex-col justify-center bg-white p-4 rounded-lg ">
         <h1 className=" flex justify-center text-xl p-2 font-bold"> Create account</h1>
+        <a href="https://example.com">Visit Example Website</a>
         <div className="flex flex-row justify-between gap-2.5">
           <div className="flex flex-col p-1 w-32">
             <label htmlFor="firstname">First Name</label>
@@ -12,9 +13,9 @@ const page = () => {
               type="text"
               id="firstname"
               placeholder="john"
-              require
+              required
               className="rounded-md border-gray-200 border-2 p-2"
-            ></input>
+            />
            
           </div>
           <div className="flex flex-col p-1 w-32">
@@ -23,7 +24,7 @@ const page = () => {
               type="text"
               id="lastname"
               placeholder="jix"
-              require
+              required
               className="rounded-md  border-2 border-gray-200 p-2"
             />
           </div>
@@ -36,7 +37,7 @@ const page = () => {
             id="user-email"
             placeholder="example@gmail.com"
             className=" rounded-md border-2 border-gray-200 p-2  w-64"
-          ></input>
+          />
         </div>
         <div className="flex flex-col gap-0.5 p-1 ">
           <label htmlFor="user-password">Password</label>
@@ -45,14 +46,23 @@ const page = () => {
             id="user-password"
             placeholder="enter password"
             className="p-2 rounded-md border-2 border-gray-200 w-64"
-          ></input>
+          />
         </div>
-        <div className="flex justify-center items-center p-4 ">
+        <div className="flex flex-col gap-0.5 p-1 ">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            type="password"
+            id="confirm-password"
+            placeholder="enter confirm password"
+            className="p-2 rounded-md border-2 border-gray-200 w-64"
+          />
+        </div>
 
+        <div className="flex justify-center items-center p-4 ">
         <button className="flex justify-center items-center bg-blue-600 w-fit px-2.5 py-2 rounded-md font-bold text-white hover:cursor-pointer">Create account</button>
         </div>
 
-        <div className="flex "></div>
+       
       </form>
     </main>
   );
