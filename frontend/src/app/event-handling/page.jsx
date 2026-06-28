77eb259df9963ebb7c4c236b95a651d8893c89bd
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-28 23:24:57 +0530

    add

--- frontend/src/app/event-handling/page.jsx
+++ frontend/src/app/event-handling/page.jsx
@@ -4,12 +4,7 @@ import React from 'react'
 
 const page = () => {
 
-const handleClick = ()=>{
-    console.log("button clicked");
-};
-const handleClick2 = (e)=>{
-    console.log("hello world",e.target);
-};
+
     
   return (
     <div className="flex item-center justify-center min-h-screen">
