a38e8b4f275bdf40b7d125911f5dde1d9d18276e
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-28 20:42:08 +0530

    add

--- frontend/src/app/event-handling/page.jsx
+++ frontend/src/app/event-handling/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const page = () => {
 
-const handelClick = ()=>{
+const handleClick = ()=>{
     console.log("button clicked");
 };
     
@@ -12,7 +12,7 @@ const handelClick = ()=>{
     <div className="flex justify-center item-center min-h-screen">
         <div className="flex items-center justify-center"> 
 
-        <button onClick={handelClick} className=" flex items-center justify-center px-2.5  border-2 border-black rounded-md hover:cursor-pointer bg-blue-800 text-white text-2xl">submit</button>
+        <button onClick={handleClick} className=" flex items-center justify-center px-2.5  border-2 border-black rounded-md hover:cursor-pointer bg-blue-800 text-white text-2xl">submit</button>
         </div>
     </div>
   )
