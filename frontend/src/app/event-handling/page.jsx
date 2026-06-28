3e3001a8f7d76b28c73ea848bb53d741626ceb0c
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-28 20:52:09 +0530

    add

--- frontend/src/app/event-handling/page.jsx
+++ frontend/src/app/event-handling/page.jsx
@@ -9,7 +9,7 @@ const handleClick = ()=>{
 };
     
   return (
-    <div className="flex justify-center item-center min-h-screen">
+    <div className="flex justify-center items-center-center min-h-screen">
         <div className="flex items-center justify-center"> 
 
         <button onClick={handleClick} className=" flex items-center justify-center px-2.5  border-2 border-black rounded-md hover:cursor-pointer bg-blue-800 text-white text-2xl">submit</button>
