5596a5544a893f6ff76dfa6da2740a87544e9f81
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-28 20:39:02 +0530

    add

--- frontend/src/app/event-handling/page.jsx
+++ frontend/src/app/event-handling/page.jsx
@@ -1,15 +1,23 @@
 'use client'
 import React from 'react'
 
+
 const page = () => {
+
+    const handelClick = ()=>{
+        console.log("button Clicked");
+    };
+
+    }
   return (
     <div className="flex justify-center item-center min-h-screen">
         <div className="flex items-center justify-center"> 
 
-        <button className=" flex items-center justify-center px-2.5  border-2 border-black rounded-md hover:cursor-pointer bg-blue-800 text-white text-2xl">submit</button>
+        <button onClick={handelClick} className=" flex items-center justify-center px-2.5  border-2 border-black rounded-md hover:cursor-pointer bg-blue-800 text-white text-2xl">submit</button>
         </div>
     </div>
   )
 }
 
 export default page;
+
