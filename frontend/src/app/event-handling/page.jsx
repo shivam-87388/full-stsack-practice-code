5fb9bbb79ef0a6ad3e1dca71bf5666df10ebdb21
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-28 21:07:25 +0530

    add

--- frontend/src/app/event-handling/page.jsx
+++ frontend/src/app/event-handling/page.jsx
@@ -7,12 +7,16 @@ const page = () => {
 const handleClick = ()=>{
     console.log("button clicked");
 };
+const handleClick2 = (e)=>{
+    console.log("hello world",e.target);
+};
     
   return (
     <div className="flex justify-center items-center-center min-h-screen">
         <div className="flex items-center justify-center"> 
 
         <button onClick={handleClick} className=" flex items-center justify-center px-2.5  border-2 border-black rounded-md hover:cursor-pointer bg-blue-800 text-white text-2xl">submit</button>
+        <button onClick={handleClick2} className=" flex items-center justify-center px-2.5  border-2 border-black rounded-md hover:cursor-pointer bg-blue-800 text-white text-2xl">submit</button>
         </div>
     </div>
   )
