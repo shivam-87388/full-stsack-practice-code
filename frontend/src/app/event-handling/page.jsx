07edc8e45d88a23a5e0491224e3eab0a602f2331
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-28 19:58:01 +0530

    add

---
+++ frontend/src/app/event-handling/page.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="flex  justify-center item-center min-h-screen">
+        <button className="flex justify-center items-center px-2.5 py-2 border-2 border-black ">submit</button>
+    </div>
+  )
+}
+
+export default page;
