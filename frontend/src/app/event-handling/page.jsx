6d4199b45edfa54398ea0a476b3cb6ad23df05d5
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-28 20:32:53 +0530

    add

--- frontend/src/app/event-handling/page.jsx
+++ frontend/src/app/event-handling/page.jsx
@@ -6,7 +6,7 @@ const page = () => {
     <div className="flex justify-center item-center min-h-screen">
         <div className="flex items-center justify-center"> 
 
-        <button className=" flex items-center justify-center px-2.5  border-2 border-black rounded-md hover:pointer-c">submit</button>
+        <button className=" flex items-center justify-center px-2.5  border-2 border-black rounded-md hover:cursor-pointer bg-blue-800 text-white text-2xl">submit</button>
         </div>
     </div>
   )
