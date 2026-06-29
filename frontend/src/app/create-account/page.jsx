cbf659212f9057ab6e9fba37e99275fa3c547d2a
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-29 21:35:00 +0530

    add

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -4,13 +4,13 @@ const page = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <form className="flex flex-col items-center justify-center p-4 bg-green-400 rounded-2xl">
-      <h1 className="text-white text-2xl font-bold"> Create Account</h1>
-        <div className="flex flex-row items-center justify-center gap-2 ">
-          <div className="flex flex-col items justify-center w-1/3"> 
+      <h1 className="text-white text-2xl font-bold mb-2.5"> Create Account</h1>
+        <div className="grid grid-cols-2  gap-3 w-full ">
+          <div className="flex flex-col "> 
           <label htmlFor='firstname'className="text-white text-xl">First name</label>
           <input id="firstname" type="text" className="text-xl rounded-md border-2 border-white px-2.5 py-1 "/> 
           </div>
-          <div className="flex flex-col"> 
+          <div className="flex flex-col "> 
           <label htmlFor='lastname'className="text-white text-xl">Last name</label>
           <input id="lastname" type="text" className="text-xl rounded-md border-2 border-white px-2.5 py-1"/> 
           </div>
