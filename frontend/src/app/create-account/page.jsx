e3baeb2d19c8f5309d61634b0242aba3b68e913b
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-29 23:28:36 +0530

    add

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -8,25 +8,25 @@ const page = () => {
         <div className="flex flex-row gap-3 w-full ">
           <div className="flex flex-col"> 
           <label htmlFor='firstname'className="text-white text-xl">First name</label>
-          <input id="firstname" type="text" className="text-xl rounded-md border-2 border-white px-2.5 py-1 "/> 
+          <input placeholder="jhon" id="firstname" type="text" required className="text-xl rounded-md border-2 border-white px-2.5 py-1 "/> 
           </div>
           <div className="flex flex-col "> 
-          <label tmlFor='lastname'className="text-white text-xl">Last name</label>
-          <input id="lastname" type="text" className="text-xl rounded-md border-2 border-white px-2.5 py-1"/> 
+          <label htmlFor='lastname'className="text-white text-xl">Last name</label>
+          <input placeholder="park" id="lastname" type="text" className="text-xl rounded-md border-2 border-white px-2.5 py-1"/> 
           </div>
         
         </div>
         <div className="flex flex-col  justify-center w-full">
         <label htmlFor='email' className="text-xl text-white">Email</label>
-        <input className="border-2 text-white text-xl border-white rounded-md px-2.5 py-1"/>
+        <input placeholder="xyz@gmail.com" id="email" required className="border-2 text-white text-xl border-white rounded-md px-2.5 py-1"/>
         </div>
         <div className="flex flex-col justify-center w-full mb-3">
         <label htmlFor='password' className="text-xl text-white">Password</label>
-        <input  placeholder="enter password" className="border-2 text-xl text-white border-white rounded-md px-2.5 py-1"/>
+        <input  placeholder="enter password" id="password" required className="border-2 text-xl text-white border-white rounded-md px-2.5 py-1"/>
         </div>
         <div className="flex flex-col justify-center w-full mb-3">
         <label htmlFor='confirmpassword' className="text-xl text-white">confirm Password</label>
-        <input placeholder="enter confirm password"className="border-2 text-xl text-white border-white rounded-md px-2.5 py-1"/>
+        <input placeholder="enter confirm password" required className="border-2 text-xl text-white border-white rounded-md px-2.5 py-1"/>
         </div>
         <button className="bg-yellow-300 px-2.5 py-2 rounded-md font-bold text-white text-xl" >submit</button>
        </form>
