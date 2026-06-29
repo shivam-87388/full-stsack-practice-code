1b8563c3acbfc94adb31083bf577b3e3f7dac1bc
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-29 20:47:26 +0530

    add

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -4,12 +4,11 @@ const page = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <form className="flex items-center justify-center p-4 bg-green-400 rounded-2xl">
-        <form
-        <label htmlFor='email' className="text-white text-xl">
-          Email
-        </label>
-        <input />
-      </form>
+        <div className="flex flex-col  justify-center">
+        <label htmlFor='email' className="text-xl text-white">Email</label>
+        <input className=/>
+        </div>
+       </form>
     </div>
   )
 }
