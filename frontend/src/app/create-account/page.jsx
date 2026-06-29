3efc897cc80963fc7b3d58413d1be709c904333c
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-29 20:43:34 +0530

    add

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 const page = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="flex items-center justify-center p-4 bg-green-400">
+      <form className="flex items-center justify-center p-4 bg-green-400 rounded-2xl">
+        <form
         <label htmlFor='email' className="text-white text-xl">
           Email
         </label>
-      </div>
+        <input />
+      </form>
     </div>
   )
 }
