f59c1d705b13826e7cf34edecc7e69f91f8f9c18
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-29 23:17:36 +0530

    add

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -5,13 +5,13 @@ const page = () => {
     <div className="flex items-center justify-center min-h-screen">
       <form className="flex flex-col items-center justify-center p-4 bg-green-400 rounded-2xl">
       <h1 className="text-white text-2xl font-bold mb-2.5"> Create Account</h1>
-        <div className="grid grid-cols-2  gap-3 w-full ">
-          <div className="flex flex-col "> 
+        <div className="flex flex-row gap-3 w-full ">
+          <div className="flex flex-col"> 
           <label htmlFor='firstname'className="text-white text-xl">First name</label>
           <input id="firstname" type="text" className="text-xl rounded-md border-2 border-white px-2.5 py-1 "/> 
           </div>
           <div className="flex flex-col "> 
-          <label htmlFor='lastname'className="text-white text-xl">Last name</label>
+          <label tmlFor='lastname'className="text-white text-xl">Last name</label>
           <input id="lastname" type="text" className="text-xl rounded-md border-2 border-white px-2.5 py-1"/> 
           </div>
         
@@ -20,10 +20,11 @@ const page = () => {
         <label htmlFor='email' className="text-xl text-white">Email</label>
         <input className="border-2 text-white text-xl border-white rounded-md px-2.5 py-1"/>
         </div>
-        <div className="flex flex-col  justify-center">
+        <div className="flex flex-col justify-center w-full">
         <label htmlFor='password' className="text-xl text-white">Password</label>
         <input className="border-2 text-xl text-white border-white rounded-md px-2.5 py-1"/>
         </div>
+        <button className="" >submit</button>
        </form>
     </div>
   )
