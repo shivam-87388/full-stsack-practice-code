fb5dfe4c184c292a2bbe7338e7abe815f425a94c
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-30 23:42:59 +0530

    add

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -29,20 +29,20 @@ const createaccount = useFormik({
           </div>
           <div className="flex flex-col "> 
           <label htmlFor='lastname'className="text-white text-xl">Last name</label>
-          <input onChange={handleChange} placeholder="park" id="lastname" type="text" className="text-xl text-white rounded-md border-2 border-white px-2.5 py-1 w-44"/> 
+          <input onChange={handleChange} name="lastname" placeholder="park" id="lastname" type="text" className="text-xl text-white rounded-md border-2 border-white px-2.5 py-1 w-44"/> 
           </div>
         
         </div>
         <div className="flex flex-col  justify-center w-full">
         <label htmlFor='email' className="text-xl text-white">Email</label>
-        <input onChange={handleChange} placeholder="xyz@gmail.com" id="email" required className="border-2 text-white text-xl border-white rounded-md px-2.5 py-1"/>
+        <input onChange={handleChange} name="email" placeholder="xyz@gmail.com" id="email" required className="border-2 text-white text-xl border-white rounded-md px-2.5 py-1"/>
         </div>
         <div className="flex flex-col justify-center w-full mb-3">
         <label htmlFor='password' className="text-xl text-white">Password</label>
-        <input onChange={handleChange} placeholder="enter password" id="password" required className="border-2 text-xl text-white border-white rounded-md px-2.5 py-1"/>
+        <input onChange={handleChange} name="password" placeholder="enter password" id="password" required className="border-2 text-xl text-white border-white rounded-md px-2.5 py-1"/>
         </div>
         <div className="flex flex-col justify-center w-full mb-3">
-        <label htmlFor='confirmpassword' className="text-xl text-white">Confirm Password</label>
+        <label htmlFor="confirmpassword" name="confirmpassword" className="text-xl text-white">Confirm Password</label>
         <input onChange={handleChange} placeholder="enter confirm password" required className="border-2 text-xl text-white border-white rounded-md px-2.5 py-1"/>
         </div>
         <button className="bg-yellow-300 px-2.5 py-2 rounded-md font-bold text-white text-xl" >submit</button>
