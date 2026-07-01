15a161352d25683b31fe4f78c7b9a2d90d2d2fc0
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-02 00:25:21 +0530

    add

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -41,24 +41,24 @@ import React from 'react'
 const page = () => {
   return (
    <div className="min-h-screen w-full   bg-white inline-flex justify-center items-center gap-2.5">
-  <div className="w-80 h-[528px] max-w-80 min-w-80 px-6 pt-14 pb-24 bg-zinc-300 rounded-3xl inline-flex flex-col justify-start items-start gap-2.5">
+  <div className="w-80 h-132 max-w-80 min-w-80 px-6 pt-14 pb-24 bg-zinc-300 rounded-3xl inline-flex flex-col justify-start items-start gap-2.5">
     <div className="self-stretch flex flex-col justify-start items-center gap-20">
-      <h1 className="self-stretch text-center justify-start text-black text-xl font-bold font-['Inter']">Login </h1>
-      <div className="self-stretch flex flex-col justify-start items-center gap-24">
+      <h1 className="self-stretch text-center justify-start text-black text-2xl font-bold font-['Inter']">Login </h1>
+      <form className="self-stretch flex flex-col justify-start items-center gap-24">
         <div className="self-stretch flex flex-col justify-start items-start gap-6">
           <div className="self-stretch flex flex-col justify-start items-start">
-            <label className="self-stretch justify-start text-black text-xl font-bold font-['Inter']">Email </label>
-            <input className="self-stretch h-8 bg-zinc-300 rounded-md border border-black" />
+            <label htmlFor="email" className="self-stretch justify-start text-black text-xl font-bold font-['Inter']">Email </label>
+            <input id="email" className="self-stretch h-8 bg-zinc-300 rounded-md border border-black" />
           </div>
           <div className="self-stretch flex flex-col justify-start items-start">
-            <div className="self-stretch justify-start text-black text-xl font-bold font-['Inter']">Password </div>
-            <div className="self-stretch h-8 bg-zinc-300 rounded-md border border-black" />
+            <label htmlFor="password"  className="self-stretch justify-start text-black text-xl font-bold font-['Inter']">Password </label>
+            <input id="password" className="self-stretch h-8 bg-zinc-300 rounded-md border border-black" />
           </div>
         </div>
         <button className="flex justify-center items-center w-full px-4 py-2.5 bg-blue-800 rounded-[5px] gap-2.5 text-white text-xl font-bold font-['Inter'] ">Log in
         
         </button>
-      </div>
+      </form>
     </div>
   </div>
 </div>
