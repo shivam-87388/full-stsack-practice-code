871e21b5a9aded6371f978e2fb38b4714871e122
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-01 21:21:54 +0530

    add

---
+++ frontend/src/app/login/page.jsx
@@ -0,0 +1,37 @@
+/*
+ * Add this to your HTML head:
+ * <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+ */
+
+function ExtractedItem({
+  text10
+}: any) {
+  return <div className="w-[272px] h-full relative self-stretch grow-0 shrink-0 flex flex-col bg-[rgba(0,0,0,0)]">
+            <span className="w-[272px] relative self-stretch grow-0 shrink-0 text-[clamp(14px,125%,20px)] font-bold leading-[24.204544067382812px] text-left text-black m-0">{text10}</span>
+            <svg className="w-[272px] h-full relative self-stretch grow-0 shrink-0 rounded-[6px]" xmlns="http://www.w3.org/2000/svg" width={272} height={32} viewBox="-1 -1 274 34" preserveAspectRatio="none">
+              <path d="M0 6C0 2.68629 2.68629 0 6 0L266 0C269.314 0 272 2.68629 272 6L272 26C272 29.3137 269.314 32 266 32L6.00001 32C2.6863 32 0 29.3137 0 26L0 6Z" fill="rgb(217, 217, 217)" fillRule="nonzero" />
+              <path d="M6 0L6 1L266 1L266 0L266 -1L6 -1L6 0ZM272 6L271 6L271 26L272 26L273 26L273 6L272 6ZM266 32L266 31L6.00001 31L6.00001 32L6.00001 33L266 33L266 32ZM0 26L1 26L1 6L0 6L-1 6L-1 26L0 26ZM6.00001 32L6.00001 31C3.23858 31 1 28.7614 1 26L0 26L-1 26C-1 29.866 2.13401 33 6.00001 33L6.00001 32ZM272 26L271 26C271 28.7614 268.761 31 266 31L266 32L266 33C269.866 33 273 29.866 273 26L272 26ZM266 0L266 1C268.761 1 271 3.23858 271 6L272 6L273 6C273 2.13401 269.866 -1 266 -1L266 0ZM6 0L6 -1C2.13401 -1 -1 2.13401 -1 6L0 6L1 6C1 3.23858 3.23858 1 6 1L6 0Z" fill="rgb(0, 0, 0)" fillRule="nonzero" />
+            </svg>
+          </div>;
+}
+const Desktop1Component = () => {
+  return <div className="relative w-full min-h-[789px] flex flex-row justify-center items-center gap-[10px] bg-white px-[512px] py-[248px]">
+  <div className="w-[320px] h-[528px] relative grow-0 shrink-0 flex flex-col gap-[10px] bg-[rgba(217,217,217,1)] pt-[56px] pb-[92px] px-[24px] rounded-[21px]">
+    <div className="w-[272px] h-full relative self-stretch grow-0 shrink-0 flex flex-col items-center gap-[80px] bg-[rgba(0,0,0,0)]">
+      <span className="w-[272px] relative self-stretch grow-0 shrink-0 text-[clamp(14px,125%,20px)] font-bold leading-[24.204544067382812px] text-center text-black m-0">Login </span>
+      <div className="w-[272px] h-full relative self-stretch grow-0 shrink-0 flex flex-col items-center gap-[96px] bg-[rgba(0,0,0,0)]">
+        <div className="w-[272px] h-full relative self-stretch grow-0 shrink-0 flex flex-col gap-[24px] bg-[rgba(0,0,0,0)]">
+          <ExtractedItem text10={"Email"} />
+          <ExtractedItem text10={"Password"} />
+        </div>
+        <div className="w-[90px] h-[44px] relative grow-0 shrink-0 flex flex-row justify-center items-center gap-[10px] bg-[rgba(34,76,191,1)] px-[17.5px] py-[10px] rounded-[5px]">
+          <span className="w-[55px] relative grow-0 shrink-0 text-[clamp(14px,125%,20px)] font-bold leading-[24.204544067382812px] text-left text-white m-0">Login</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>;
+};
+export default Desktop1Component;
