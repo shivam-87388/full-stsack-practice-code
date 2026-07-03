28d31f0aba717f712dbde52405203a2e5605a569
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-03 23:12:01 +0530

    add

--- backend/router/todoRouter.js
+++ backend/router/todoRouter.js
@@ -2,7 +2,10 @@ import express from "express";
 import todo from "../models/todo.js"
 
 const todorouter = express();
-
+ //get method
 todorouter.get("/todo",(req,res)=>{
     res.send("sucessfull sned result");
-})
+});
+todorouter.post("/todo",(req,res)=>{
+    res.send("sucessfull sned result");
+});
