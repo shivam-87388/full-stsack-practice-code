7260cfe55c2e0a17ae9b2ffd9eb9702e688dc6a0
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-04 23:34:39 +0530

    add

--- frontend/src/app/calculator/page.jsx
+++ frontend/src/app/calculator/page.jsx
@@ -7,22 +7,12 @@ const page = () => {
     <div className="w-full min-h-screen p-2.5 bg-zinc-400 inline-flex justify-center items-center gap-2.5 overflow-hidden">
   <div className="w-56 h-116 max-w-56 pt-16 pb-2.5 bg-white rounded-xl flex justify-center items-center gap-2.5">
     <div className="flex-1 h-64 py-2 bg-white flex flex-col justify-center items-center gap-3.5">
+        <div className="flex flex-row">
+
+        </div>
       
-<div className="flex flex-row justify-center items-center gap-3.5">
-  <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] flex flex-col justify-center items-center gap-2.5">
-    <div className=" text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">7</div>
-  </div>
-  <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] flex flex-col justify-center items-center gap-2.5">
-    <div className=" text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">8</div>
-  </div>
-  <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5">
-    <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">9</div>
-  </div>
-  <div className="w-8 h-9 px-1.5 py-1.5 bg-zinc-100 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5 overflow-hidden">
-    <div className="w-6 h-6 relative overflow-hidden">
-      <div className="w-3 h-3 left-[6px] top-[6px] absolute outline outline-2 outline-offset-[-1px] outline-black" />
-    </div>
-  </div>
+<div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] flex flex-col justify-center items-center gap-2.5 text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">
+  <div className="text-center justify-start ">AC</div>
 </div>
 
       <div className="self-stretch inline-flex justify-center items-center gap-3.5">
