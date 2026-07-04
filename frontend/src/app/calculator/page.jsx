7aa5b54cbdc84c5319e1e7ab09670941695a2635
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-04 22:19:03 +0530

    add

--- frontend/src/app/calculator/page.jsx
+++ frontend/src/app/calculator/page.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="w-[1440px] h-[1024px] p-2.5 bg-zinc-400 inline-flex justify-center items-center gap-2.5 overflow-hidden">
-  <div className="w-56 h-[464px] max-w-56 pt-16 pb-2.5 bg-white rounded-xl flex justify-center items-center gap-2.5">
+    <div className="w-full min-h-screen p-2.5 bg-zinc-400 inline-flex justify-center items-center gap-2.5 overflow-hidden">
+  <div className="w-56 h-116 max-w-56 pt-16 pb-2.5 bg-white rounded-xl flex justify-center items-center gap-2.5">
     <div className="flex-1 h-64 py-2 bg-white inline-flex flex-col justify-center items-center gap-3.5">
       <div className="self-stretch inline-flex justify-center items-center gap-3.5">
         <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5">
@@ -13,11 +13,11 @@ const page = () => {
           <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">%</div>
         </div>
         <div className="w-8 h-9 px-1.5 py-1.5 bg-zinc-100 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5 overflow-hidden">
-          <div className="self-stretch h-4 outline outline-2 outline-offset-[-1px] outline-black" />
+          <div className="self-stretch h-4 outline  outline-black" />
         </div>
         <div className="w-8 h-9 px-1.5 py-1.5 bg-zinc-100 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5 overflow-hidden">
           <div className="w-6 h-6 relative overflow-hidden">
-            <div className="w-3.5 h-3.5 left-[5px] top-[5px] absolute outline outline-2 outline-offset-[-1px] outline-black" />
+            <div className="w-3.5 h-3.5 left-1.25 top-1.25 absolute outline-2 outline-offset-1 outline-black" />
           </div>
         </div>
       </div>
@@ -33,7 +33,7 @@ const page = () => {
         </div>
         <div className="w-8 h-9 px-1.5 py-1.5 bg-zinc-100 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5 overflow-hidden">
           <div className="w-6 h-6 relative overflow-hidden">
-            <div className="w-3 h-3 left-[6px] top-[6px] absolute outline outline-2 outline-offset-[-1px] outline-black" />
+            <div className="w-3 h-3 left-1.5 top-1.5 absolute outline-2 outline-offset-1 outline-black" />
           </div>
         </div>
       </div>
