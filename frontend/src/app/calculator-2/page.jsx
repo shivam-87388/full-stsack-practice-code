279f3113741d9e2e9988b4abce6bf3eae45d809a
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-05 21:37:58 +0530

    add

--- frontend/src/app/calculator-2/page.jsx
+++ frontend/src/app/calculator-2/page.jsx
@@ -6,10 +6,10 @@ const page = () => {
   return (
    <div className="w-full min-h-screen flex flex-col items-center justify-center">
 
-<div className="shadow-md"> 
+<div className="shadow-md p-4 "> 
   
  {/* row-1 */}
-    <div className="flex flex-row gap-3.5 p-2 ">
+    <div className="flex flex-row gap-3.5 p-2">
       <button className="flex-1 w-24 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">AC</button>
       <button className=" flex-1 w-24  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">%</button>
       <button className="flex-1 flex items-center justify-center w-24 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"><IconBackspace/></button>
