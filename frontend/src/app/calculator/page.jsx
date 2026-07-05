f58274f1f78ef8adcc7ec39f32969880c7313e86
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-06 00:09:22 +0530

    add

--- frontend/src/app/calculator/page.jsx
+++ frontend/src/app/calculator/page.jsx
@@ -1,90 +1,64 @@
+'use client'
 import React from 'react'
-import { IconDivide , IconX , IconBackspace } from '@tabler/icons-react';
+import { IconDivide , IconX , IconBackspace , IconMinus , IconPlus, IconEqual } from '@tabler/icons-react';
 
 
 const page = () => {
   return (
-    <div className="w-full min-h-screen p-2.5 bg-zinc-400 flex justify-center items-center gap-2.5 overflow-hidden">
-   <div className="w-56 h-116 max-w-56 pt-16 pb-2.5 bg-white rounded-xl inline-flex justify-center items-center gap-2.5">
-  <div className="flex-1 h-64 py-2 bg-white inline-flex flex-col justify-center items-center gap-3.5">
-    <div className="self-stretch inline-flex justify-center items-center gap-3.5">
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5">
-        <div className="text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">AC</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">%</div>
-      </div>
-      <div className="w-8 h-full px-1.5 py-1.5 bg-zinc-100 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5 overflow-hidden">
-        <IconBackspace stroke={2}/>
-      </div>
-      <div className="w-8 h-9 px-1.5 py-1.5 bg-zinc-100 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5 overflow-hidden">
-        <IconDivide stroke={2}/>
-      </div>
+   <div className="w-full min-h-screen flex flex-col items-center justify-center p-5">
+
+<div className="shadow-[0px_0px_35px_1px_rgba(0,0,0,0.25)] p-1.5 rounded-lg"> 
+  <input className="text-end w-full h-24 text-3xl border-2 border-black p-2 text-black font-bold rounded-lg mb-2"/>
+  
+ {/* row-1 */}
+    <div className="flex flex-row gap-3.5 p-2 rounded-md">
+      <button className="flex-1 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">AC</button>
+      <button className=" flex-1 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">%</button>
+      <button className="flex-1 flex items-center justify-center bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"><IconBackspace/></button>
+      <button className="flex-1  flex items-center justify-center bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"><IconDivide/></button>
     </div>
-    <div className="self-stretch inline-flex justify-center items-center gap-3.5">
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">7</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">8</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">9</div>
-      </div>
-      <div className="w-8 h-full px-1.5 py-1.5 bg-zinc-100 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-2.5 overflow-hidden">
-        <IconX stroke={2}/>
-      </div>
+     {/* row-2 */}
+    <div className="flex flex-row gap-3.5  p-2 ">
+      <button className="flex-1 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">7</button>
+      <button className=" flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">8</button>
+      <button className="flex-1 flex items-center justify-center bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">9</button>
+      <button className="flex-1  flex items-center justify-center bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"><IconX/></button>
     </div>
-    <div className="self-stretch inline-flex justify-center items-center gap-3.5">
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">4</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">5</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">6</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-zinc-100 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="self-stretch text-center justify-center text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">-</div>
-      </div>
+    {/* row-3 */}
+    <div className="flex flex-row gap-3.5  p-2 ">
+      <button className="flex-1 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">4</button>
+      <button className=" flex-1 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">5</button>
+      <button className="flex-1 flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">6</button>
+      <button className="flex-1  flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"><IconMinus/></button>
     </div>
-    <div className="self-stretch inline-flex justify-center items-center gap-3.5">
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">1</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">2</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">3</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">+</div>
-      </div>
+ {/* row-4 */}
+    <div className="flex flex-row gap-3.5  p-2 ">
+      <button className="flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">1</button>
+      <button className=" flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">2</button>
+      <button className="flex-1 flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">3</button>
+      <button className="flex-1  flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"><IconPlus/></button>
     </div>
-    <div className="self-stretch inline-flex justify-center items-center gap-3.5">
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">00</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">0</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-neutral-200/40 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="self-stretch text-center justify-start text-black text-xl font-normal font-['Arial_Rounded_MT_Bold']">.</div>
-      </div>
-      <div className="w-8 px-1.5 py-1.5 bg-orange-500 rounded-lg shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] inline-flex flex-col justify-center items-center gap-3.5">
-        <div className="self-stretch text-center justify-start text-white text-xl font-normal font-['Arial_Rounded_MT_Bold']">=</div>
-      </div>
+      {/* row-5 */}
+    <div className="flex flex-row gap-3.5  p-2 ">
+      <button className="flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">00</button>
+      <button className=" flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">0</button>
+      <button className="flex-1 flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">.</button>
+      <button className="flex-1  flex items-center justify-center  bg-[#FF7700] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"><IconEqual color="white"/></button>
     </div>
-  </div>
-</div>  
+    </div>
+    
+
 
 </div>
+ 
+   
 
+   
+  
+   
 
 
   )
 }
 
-export default page
+export default page;
