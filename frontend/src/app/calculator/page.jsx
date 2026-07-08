3df4eaaf6fdb5c7471ae177141ea8eec2396f92e
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-09 00:14:05 +0530

    add

--- frontend/src/app/calculator/page.jsx
+++ frontend/src/app/calculator/page.jsx
@@ -4,12 +4,12 @@ import { IconDivide , IconX , IconBackspace , IconMinus , IconPlus, IconEqual } 
 
 
 const page = () => {
-  const [value, setValue] = useState("");
+  const [value, setvalues] = useState("");
   
 
   const handleClick =(e)=>{
-    setValue(e.target.Value);
-    console.log(e.target.Value);
+    setvalues(e.target.value);
+    console.log(e.target.value);
     
   }
 
@@ -28,29 +28,29 @@ const page = () => {
     </div>
      {/* row-2 */}
     <div className="flex flex-row gap-3.5  p-2 ">
-      <button value="Value" onClick={()=>{handleClick("7")}} className="flex-1 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">7</button>
-      <button value="Value" onClick={()=>{handleClick("8") }} className="flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">8</button>
-      <button value="Value" onClick={()=>{handleClick("9")}} className="flex-1 flex items-center justify-center bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">9</button>
+      <button values="values" onClick={()=>{handleClick("7")}} className="flex-1 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">7</button>
+      <button values="values" onClick={()=>{handleClick("8") }} className="flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">8</button>
+      <button values="values" onClick={()=>{handleClick("9")}} className="flex-1 flex items-center justify-center bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">9</button>
       <button onClick={()=>{handleClick("*")}} className="flex-1  flex items-center justify-center bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"><IconX/></button>
     </div>
     {/* row-3 */}
     <div className="flex flex-row gap-3.5  p-2 ">
-      <button value="4" onClick={()=>{handleClick("4")}} className="flex-1 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">4</button>
-      <button value="Value" onClick={()=>{handleClick("5")}} className=" flex-1 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">5</button>
-      <button value="Value" onClick={()=>{handleClick("6")}} className="flex-1 flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">6</button>
+      <button values="4" onClick={()=>{handleClick("4")}} className="flex-1 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">4</button>
+      <button values="values" onClick={()=>{handleClick("5")}} className=" flex-1 bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">5</button>
+      <button values="values" onClick={()=>{handleClick("6")}} className="flex-1 flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">6</button>
       <button onClick={()=>{handleClick("-")}} className="flex-1  flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"><IconMinus/></button>
     </div>
  {/* row-4 */}
     <div className="flex flex-row gap-3.5  p-2 ">
-      <button value="Value" onClick={()=>{handleClick("1")}} className="flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">1</button>
-      <button value="Value" onClick={()=>{handleClick("2")}} className=" flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">2</button>
-      <button value="Value" onClick={()=>{handleClick("3")}} className="flex-1 flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">3</button>
+      <button values="Valu" onClick={()=>{handleClick("1")}} className="flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">1</button>
+      <button values="values" onClick={()=>{handleClick("2")}} className=" flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">2</button>
+      <button values="values" onClick={()=>{handleClick("3")}} className="flex-1 flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">3</button>
       <button onClick={()=>{handleClick("+")}} className="flex-1  flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"><IconPlus/></button>
     </div>
       {/* row-5 */}
     <div className="flex flex-row gap-3.5  p-2 ">
-      <button value="Value" onClick={()=>{handleClick("00")}} className="flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">00</button>
-      <button value="Value" onClick={()=>{handleClick("0")}} className=" flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">0</button>
+      <button values="values" onClick={()=>{handleClick("00")}} className="flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">00</button>
+      <button values="values" onClick={()=>{handleClick("0")}} className=" flex-1  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">0</button>
       <button onClick={()=>{handleClick(".")}} className="flex-1 flex items-center justify-center  bg-[#DFDFDF] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]">.</button>
       <button onClick={()=>{handleClick("=")}} className="flex-1  flex items-center justify-center  bg-[#FF7700] px-4 py-2.5 rounded-lg text-xl font-bold shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"><IconEqual color="white"/></button>
     </div>
