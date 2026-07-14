c63be4b5678da6e5583ba912c8ed965b04a49d1c
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-14 11:40:42 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,4 @@
-import express from "express";
-import dotenv from "dotenv";
-import connect_db from "./config/connection.js";
-import todoRouter from "./router/todoRouter.js";
-//import userRouter from "./router/userRouter.js";
-import cors from 'cors'
+const express = require("express");
 
-
-dotenv.config();
-connect_db();
-const app =  express();
+const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
-app.use("/todo",todoRouter);
-
-app.listen(PORT,()=>{
-  console.log(`server connected ${PORT}`);
-});
--- backend/models/todo.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-const todoSchema = new mongoose.Schema({
-    todo: {type:String, required:true},
-});
-export default todoSchema;
