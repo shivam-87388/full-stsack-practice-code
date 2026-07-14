d599a7b198a7391c139c8c8b13af1d02d99c8749
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-14 23:35:04 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const connection_db = require("./config/connection");
 const cors = require('cors')
+
 require("dotenv").config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,7 +12,7 @@ connection_db();
 app.use(cors())
 app.use(express.json());
 
-app.use("/todo",todoRouter)
+app.use("/todo",require("./router/todoRouter"));
 
 
 
