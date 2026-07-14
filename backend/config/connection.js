fb79fd5fb15ec9ec55446bed509124c7273e1175
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-14 12:00:22 +0530

    add

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,2 +1,13 @@
 const mongoose = require('mongoose'); 
-const db = mongoose.connection(process.env.MONGODB_URI);
+const connection_db = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("databse connected sucessfully")
+    }
+    catch (error){
+        console.log("database connection failed", error.message);
+
+    }
+    
+};
+module.exports = connection_db;
