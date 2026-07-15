0e0e1c0981608391a034bdb8eb05550c579a228e
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-15 22:48:25 +0530

    add

---
+++ backend/models/todo.js
@@ -0,0 +1,3 @@
+import mongoose from 'mongoose';
+const  todo = new mongoose.Schema({ name: String, requ });
+module.exports =  todo;
--- backend/router/todoRouter.js
+++ backend/router/todoRouter.js
@@ -7,5 +7,16 @@ router.get('/', (req, res) => {
     msg: "successful",
   })
 });
+//post method
+router.post('/add', async(req, res) => {
+  try{
+await res.status(200).json({message:"sucessfull post api"},(req.body))
 
+  }catch(error){
+    res.status(500).json({message: error.message});
+
+  }
+
+  
+});
 module.exports = router;
