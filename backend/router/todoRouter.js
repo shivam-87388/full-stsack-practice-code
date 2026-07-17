81fbe091940b295f4a2a8790c8b0d4ff899747a2
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-17 23:54:09 +0530

    add

--- backend/router/todoRouter.js
+++ backend/router/todoRouter.js
@@ -21,11 +21,11 @@ router.get('/', async(req, res) => {
 //post method
 router.post("/add", async(req, res) => {
   try {
-const addtodo = await todo.updateOne(req.body)
+const addtodo = await todo.create(req.body)
 
     res.status(200).json({ message: "sucessfull post api", 
     addtodo});
-    
+
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
