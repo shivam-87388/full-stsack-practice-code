e28c2935334d50c551e9eea6d6a48c446d806f43
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-22 11:12:19 +0530

    add

--- frontend/src/app/todolist/page.jsx
+++ frontend/src/app/todolist/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { IconTrash } from "@tabler/icons-react";
 import axios from 'axios';
 
