3bcf01cc683c603eccc11b92fc69c0997e8c889d
Shivam Singh <hardwellshivamsingh2002@gmail.com>
2026-07-23 21:55:38 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose');   const userSchema = new mongoose.Schema({
 firstname:{type:String, required: true},
 lastname:{type:String},
+email: {type:String, required:true},
