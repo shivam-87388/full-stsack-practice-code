2881c3baa837294566cef014cb35b9c4f34c00d5
Shivam Singh <hardwellshivamsingh2002@gmail.com>
2026-07-23 21:58:30 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,4 +4,7 @@ lastname:{type:String},
 email: {type:String, required:true},
 password:{type: String, required:true},
 })
+createdAt: {
+    type: Date,
+    default: Date.now }
 module.exports = mongoose.model('User', userschema);
