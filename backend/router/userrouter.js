b2db85a925deaa87fb8b164b450653db0fde171a
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-24 22:42:58 +0530

    add

--- backend/router/userrouter.js
+++ backend/router/userrouter.js
@@ -3,4 +3,18 @@ const user = require("../models/user")
 const router = express.Router();
 
 //get method
-router.get()
+router.get("/", async(req,res)=>{
+    try {
+        const users = user.find();
+        res.send(200).json({
+            data: users,
+        })
+    } catch (error) {
+        res.send(500),json({
+            mesage: error.message
+        })
+
+        
+    }
+
+})
