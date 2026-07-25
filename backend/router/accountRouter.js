73956f2ddf211e23b7c6257b2e090967f832c8e5
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-25 23:47:08 +0530

    add

--- backend/router/accountRouter.js
+++ backend/router/accountRouter.js
@@ -6,11 +6,11 @@ const router = express.Router();
 router.get("/login", async(req,res)=>{
     try {
         const users = user.find();
-        res.send(200).json({
+        res.status(200).json({
             data: users,
         })
     } catch (error) {
-        res.send(500),json({
+        res.status(500),json({
             mesage: error.message
         })   
     }
@@ -21,13 +21,13 @@ router.get("/login", async(req,res)=>{
 router.post("/create-account", async(req,res)=>{
     try {
         const createaccount = await user.create(req.body);
-        res.send(200).json({
+        res.status(200).json({
             message: "account successfully created",
             data: createaccount
         });
         
     } catch (error) {
-        res.send(500).json({
+        res.status(500).json({
             message: error.mesage
         });
         
