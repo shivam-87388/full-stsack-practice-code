3a3ee7a5c59ed4fe0a207402ae14cc6d805d9e32
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-29 00:14:26 +0530

    add

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -16,28 +16,16 @@ const loginform = useFormik({
   
 });
   return (
-   <div className="min-h-screen w-full bg-white inline-flex justify-center items-center gap-2.5">
-  <div className="w-80 h-132 max-w-80 min-w-80 px-6 pt-14 pb-24 bg-zinc-300 rounded-3xl inline-flex flex-col justify-start items-start gap-2.5">
-    <div className="self-stretch flex flex-col justify-start items-center gap-20">
-      <h1 className="self-stretch text-center justify-start text-black text-2xl font-bold font-['Inter']">Login </h1>
-      <form onSubmit={loginform.handleSubmit} className="self-stretch flex flex-col justify-start items-center gap-24">
-        <div className="self-stretch flex flex-col justify-start items-start gap-6">
-          <div className="self-stretch flex flex-col justify-start items-start">
-            <label htmlFor="email" className="self-stretch justify-start text-black text-xl font-bold font-['Inter']">Email </label>
-            <input id="email" value={loginform.values.email} name="email" onChange={loginform.handleChange} type="email" placeholder="email" className="self-stretch h-8 p-2 bg-zinc-300 rounded-md border border-black" />
-          </div>
-          <div className="self-stretch flex flex-col justify-start items-start">
-            <label htmlFor="password" className="self-stretch justify-start text-black text-xl font-bold font-['Inter']">Password </label>
-            <input id="password" value={loginform.values.password} name="password" onChange={loginform.handleChange} type="password" placeholder="password" className="self-stretch h-8 p-2 bg-zinc-300 rounded-md border border-black" />
-          </div>
-        </div>
-        <button type="submit" className="flex justify-center items-center w-full px-4 py-2.5 bg-blue-800 rounded-[5px] gap-2.5 text-white text-xl font-bold font-['Inter'] ">Log in
-        
-        </button>
-      </form>
-    </div>
-  </div>
-</div>
+   <div className="flex items-center justify-center h">
+    <form className="flex flex-col items-center justify-center p-4 bg-amber-100"> 
+      <h1 className="font-bold text-xl">login form</h1>
+      <div className="flex flex-col items-center justify-center">
+      <label>email</label>
+
+      </div>
+    </form>
+
+   </div>
   )
 }
 
