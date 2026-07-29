e01e1304bec9eb0eda3b99d77c290259566c99f4
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-29 23:58:52 +0530

    add

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -1,8 +1,9 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { useFormik } from 'formik';
 
 const page = () => {
+  // const [password, setPassword]= useState(false);
 const loginform = useFormik({
   initialValues:{
     email:"",
@@ -21,13 +22,20 @@ const loginform = useFormik({
       <h1 className="font-bold text-xl">login form</h1>
       <div className="flex flex-col  justify-center mb-0.5">
       <label htmlFor="email">email</label>
-      <input type="text" id="email" className="border-2 borde-black rounded-md px-1.5 py-1"></input>
+      <input type="text" id="email" placeholder='email' className="border-2 borde-black rounded-md px-1.5 py-1 focus:outline-1 focus:outline-black">
+      
+      </input>
+    
       </div>
       <div className="flex flex-col  justify-center mb-3">
       <label htmlFor="email">password</label>
-      <input type="text" id="password" className="border-2 borde-black rounded-md px-1.5 py-1"></input>
+      <input type="password" id="password" placeholder="password" className="border-2 borde-black rounded-md px-1.5 py-1  focus:outline-1 focus:outline-black"> <span className="text-sm font-black p-1.5">show password</span></input>
+      
+      <a href='#' className='text-blue-600 text-sm text-right self-end mt-0.5'> forget password ?</a>
       </div>
-      <button className="text-xl  text-white font-light bg-blue-700 rounded-md w-full py-2 ">login</button>
+      <button type="submit" className="w-full py-2 bg-blue-700 text-white rounded-md
+             transition-transform duration-300 ease-in-out
+             hover:scale-95 hover:cursor-pointer">login</button>
     </form>
 
    </div>
