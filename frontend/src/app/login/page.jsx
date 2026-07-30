1b5ea2f49bfcb1eb3b8a1fdb9845799bb38ea1fe
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-07-30 11:46:46 +0530

    add

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -3,7 +3,14 @@ import React, { useState } from 'react'
 import { useFormik } from 'formik';
 
 const page = () => {
-  // const [password, setPassword]= useState(false);
+   const [showpassword, setPassword]= useState(false);
+
+   const handleClick = ()=>{
+    setPassword(!showpassword);
+   }
+
+  
+
 const loginform = useFormik({
   initialValues:{
     email:"",
@@ -23,9 +30,9 @@ const loginform = useFormik({
       <div className="flex flex-col  justify-center mb-0.5">
       <label htmlFor="email">email</label>
       <input type="text" id="email" placeholder='email' className="border-2 borde-black rounded-md px-1.5 py-1 focus:outline-1 focus:outline-black"></input>
-      <label htmlFor="email">password</label>
-      <input type="password" id="password" placeholder="password" className="border-2 borde-black rounded-md px-1.5 py-1  focus:outline-1 focus:outline-black"></input>
-      <button className="text-sm text-black  font-semibold rounded-md bg-gray-200 w-fit p-1.5">show</button>
+      <label htmlFor="password">password</label>
+      <input id="password" placeholder="password" type={showpassword ? "text":"password"} className="border-2 borde-black rounded-md px-1.5 py-1  focus:outline-1 focus:outline-black"></input>
+      <button type="button" onClick={handleClick}  className="text-sm text-black  font-semibold rounded-md bg-gray-200 w-fit p-1.5 hover:cursor-pointer">{showpassword ? "hide":"show"}</button>
       <a href='#' className='text-blue-600 text-sm text-right self-end mt-0.5'> forget password ?</a>
       </div>
       <button type="submit" className="w-full py-2 bg-blue-700 text-white rounded-md
