b219c8f1bbc27091c765b7e7f3d31c10f2e82dd8
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-01 22:29:23 +0530

    add

--- frontend/src/app/dark-ligth-toggle/page.jsx
+++ frontend/src/app/dark-ligth-toggle/page.jsx
@@ -3,15 +3,17 @@ import { IconSun, IconMoon } from '@tabler/icons-react';
 
 const Togglelight = () => {
   const [light,setLight] = useState(false);
-  const handleClick
-
+  const handleClick = ()=>{
+    setLight(!light);
+  }
+ 
 
 
   return (
     <div className="flex items-center justify-center  w-full h-screen ">
         <div className="flex items-center justify-center bg-amber-100 rounded-full border-2 border-black gap-2 px-3 py-2.5">
         <button type="button" onClick={handleClick} className="hover:cursor-pointer"><IconSun stroke={2} size={25} /></button>
-        <button type="button" className="hover:cursor-pointer hover:fill-black"><IconMoon stroke={2} size={25}/></button>
+        <button type="button" onClick={handleClick} className="hover:cursor-pointer hover:fill-black"><IconMoon stroke={2} size={25}/></button>
         </div>
     </div>
   )
