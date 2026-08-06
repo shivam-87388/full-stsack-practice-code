3d4ed5ee79b6f8bfdc8634254cd6e8585b861124
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-06 23:03:02 +0530

    add

--- backend/router/accountRouter.js
+++ backend/router/accountRouter.js
@@ -4,28 +4,29 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 
 // login account
-router.post('/login', async(req, res) => {
- try {
-    const hassedPasswrod = await bcrypt.compareSync(req.body.password,10);
-    const loginuser = await user.findOne({email: req.body.email})
-    if (!loginuser=== null) {
-        return res.status(400).json({
-            message: "user does not exist"
-        })
-        if (req.body.password !== loginuser.password) {
-           return res.status(400).json({
-            message: "password is wrong"
-        }) 
-        }
-        res.status(200).json({message: "login successful"})
-    }
- } catch (error) {
-    res.status(500).json({
-        message: "error.message"
-    })
+// router.post('/login', async(req, res) => {
+//  try {
     
- }
-})
+//     const loginuser = await user.findOne({email: req.body.email})
+//     if (!loginuser) {
+//         return res.status(400).json({
+//             message: "user does not exist"
+//         })
+//         const checkPasswrod = await bcrypt.compare(req.body.password,loginuser.password);
+//         if (!checkPasswrod) {
+//            return res.status(400).json({
+//             message: "password is wrong"
+//         }) 
+//         }
+//         res.status(200).json({message: "login successful"})
+//     }
+//  } catch (error) {
+//     res.status(500).json({
+//         message: "error.message"
+//     })
+    
+//  }
+// })
 
 //create account
 router.post("/create-account", async(req,res)=>{
@@ -59,4 +60,35 @@ router.post("/create-account", async(req,res)=>{
 
 })
 
+router.post('/login', async(req, res) => {
+    try {
+        const findUser = await user.findOne({email: req.body.email}),
+        if (findUser != null) {
+            const hassedPassword = await bcrypt.compare(findUser.password,req.body.password)
+            if (hassedPassword !== true) {
+                res.status(400).json({
+                    message: "password is wrong",
+                })   
+                
+            }
+            res.status(200).json({
+                    message: "login successfully"})
+
+            
+        }
+
+        
+     
+        
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+        
+    }
+  
+})
+
 module.exports = router;
+
+
