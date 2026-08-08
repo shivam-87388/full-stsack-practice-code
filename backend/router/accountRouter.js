498161869e5234c5502c7eec366a4559e2d81963
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-08 21:43:11 +0530

    add

--- backend/router/accountRouter.js
+++ backend/router/accountRouter.js
@@ -1,118 +1,90 @@
-const express = require('express');
-const user = require("../models/account")
+const express = require("express");
+const user = require("../models/account");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // login account
-router.post('/login', async(req, res) => {
+router.post("/login", async (req, res) => {
     try {
-        const findUser = await user.findOne({email: req.body.email})
+        const findUser = await user.findOne({ email: req.body.email });
         if (findUser != null) {
-            const hassedPassword = await bcrypt.compare(req.body.password,findUser.password)
+            const hassedPassword = await bcrypt.compare(
+                req.body.password,
+                findUser.password,
+            );
             if (hassedPassword == true) {
                 const payload = {
                     id: findUser._id,
-                    email: findUser.email
+                    email: findUser.email,
                 };
-                const token = jwt.sign(payload,process.env.JWT_SECRET);
-        
+                const token = jwt.sign(payload, process.env.JWT_SECRET);
+
                 res.status(200).json({
                     message: "login sucessfull",
-                    token: token
-                })   
-                
+                    token: token,
+                });
             } else {
                 res.status(404).json({
-                    message: "password wrong"
-                })   
-                
+                    message: "password wrong",
+                });
             }
         } else {
             res.status(404).json({
-                message: "user does not exist"
-            })   
-            
-        } 
-       
+                message: "user does not exist",
+            });
+        }
     } catch (error) {
         res.status(500).json({
             message: error.message,
-        })
-        
+        });
     }
-  
-})
+});
 
 //create account
-router.post("/create-account", async(req,res)=>{
-    
+router.post("/create-account", async (req, res) => {
     try {
-        
-        const existingUser = await user.findOne({email: req.body.email})
-        if (existingUser) 
-        {
-        return res.status(400).json({ message: "Email already registered" });
+        const existingUser = await user.findOne({ email: req.body.email });
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already registered" });
         }
-        const hassedPasswrod =  await bcrypt.hash(req.body.password, 10);
+        const hassedPasswrod = await bcrypt.hash(req.body.password, 10);
         const createAccount = await user.create({
             firstname: req.body.firstname,
             lastname: req.body.lastname,
             email: req.body.email,
-            password: hassedPasswrod
-    });
+            password: hassedPasswrod,
+        });
         res.status(200).json({
             message: "account successfully created",
             data: createAccount,
         });
-        
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        });
-        
-    }
-
-
-})
-
-router.post('/login', async(req, res) => {
-    try {
-        const findUser = await user.findOne({email: req.body.email})
-        if (findUser != null) {
-            const hassedPassword = await bcrypt.compare(req.body.password,findUser.password)
-            if (hassedPassword == true) {
-                res.status(200).json({
-                    message: "login sucessfull"
-                })   
-                
-            } else {
-                res.status(404).json({
-                    message: "password wrong"
-                })   
-                
-            }
-
-           
-
-        } else {
-            res.status(404).json({
-                message: "user does not exist"
-            })   
-            
-        } 
-       
     } catch (error) {
         res.status(500).json({
             message: error.message,
-        })
-        
+        });
     }
-  
-})
+});
+
+//update passwrod
+router.patch("/update/:id", async (req, res) => {
+    try {
+        const hassedPassword = await bcrypt.hash(req.body.password, 10);
+        const updateUser = await user.findByIdAndUpdate(req.params.id, {
+            password: hassedPassword,
+        });
+        if (updateUser !== null) {
+            res.status(200).json({ mesasge: "passwrod changed successfully"
+             });
+    
+        } else {
+            res.status(404).json({ mesasge: "user does not exist" });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+});
 
 module.exports = router;
-
-
